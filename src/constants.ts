1c2716b562195cc82464086464edccb0ae4e5df5
Miguel Pérez Colino <miguelp@vmware.com>
2022-05-06 11:55:41 +0200

    Modified Default Font

--- src/constants.ts
+++ src/constants.ts
@@ -67,10 +67,10 @@ export const CLASSES = {
 
 // 1-based in case we ever do `if(element.fontFamily)`
 export const FONT_FAMILY = {
-  Virgil: 1,
+  Metropolis: 1,
   Helvetica: 2,
   Cascadia: 3,
-  Metropolis: 4,
+  Virgil: 4,
 };
 
 export const THEME = {
